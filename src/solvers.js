470a6f9a76c8c6e4436361df3361c8c7a3490677
albertchanged <albertchanged@gmail.com>
2017-12-06 20:54:25 -0800

    Attempt Rook solver

--- src/solvers.js
+++ src/solvers.js
@@ -11,13 +11,52 @@
 // take a look at solversSpec.js to see what the tests are expecting
 
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+// return a matrix (an array of arrays) representing a single nxn chessboard, 
+// with n rooks placed such that none of them can attack each other
 
 
-
+  /*
+  Specification:
+    Input: number of rooks and board size
+    Output: a matrix (array of arrays)
+    Side effects: expensive
+    Edge Cases: None for now
+  Explanation:
+    - Board is representation of n rooks placed in safe positions
+  Viz (draw it):
+  Approximation (psuedocode) :
+  Verfication (go through with example data):
+  Implementation (code it):
+  */
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
-
+  // Define inner function called placeRooks(board, numRooks)
+  var placeRooks = function(board, numRooks) {
+    // iterate i to size
+    for (var i = 0; i < n; i++) {
+      // iterate j to size
+      for (var j = 0; j < n; i++) {
+        // if this.get(i)[j] === 0
+        if (this.get(i)[j] === 0) {
+          // call togglePiece(i, j)
+          this.togglePiece(i, j);
+          numRooks++;
+          // if this.hasRow === false && this.hasCol === false
+          if (!this.hasRowConflictAt(i) && !this.hasColConflictAt(j)) {
+            // if numRooks === n
+            if (numRooks === n) {
+              // return board
+              return JSON.stringify(board);
+            }
+            // recurse placeRooks(board, numRooks)
+            placeRooks(board, numRooks);
+          }
+        }
+      }
+    }
+  };
+  var b = new Board({n: n});
+  placeRooks(b, n);   
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
